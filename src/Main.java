3780a6340d238a30056b1cf48ff8b1a625212386
Cranky Bawler <6666lenia6666@mail.ru>
2024-06-25 18:55:18 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            check("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+            System.out.println("Вход выполнен успешно");
+        } catch (WrongLoginException e) {
+            e.printStackTrace();
+        } catch (WrongPasswordException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("Добрый день!");
+
+        }
+    }
+
+    public static void check (String login, String password, String confirmPassword) {
+        if (login.length() > 20 && !login.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongLoginException();
+        }
+        if (password.length() > 20 && !password.matches("[a-zA-z0-9_]+")) {
+            throw new WrongPasswordException();
+        }
+        if (confirmPassword != password) {
+            throw new WrongLoginException();
+        }
+
+
     }
 }
